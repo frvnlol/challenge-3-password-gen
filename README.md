a195f02ea8a9fa1b124e7f45a8bbad1f7819d1a6
frvnci5 <frvncis@outlook.com>
2023-05-15 01:31:19 -0700

    added prompts and sources for code used to README

---
+++ README.md
@@ -0,0 +1,7 @@
+# JavaScript Password Generator
+
+## Credits 
+
+Source I used to make sure prompt for password length only accepted numbers: 
+[parseInt](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt#try_it)
+[isNaN](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/isNaN)
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 // Variable for password length and the password array.
-var passLength = 0;
+var passLength = 8;
 var passArr = [];
 
 // Global variables for all available characters to be used in password generator. .split is used to turn the strings into arrays.
@@ -14,12 +14,37 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+      var password = generatePassword();
+      var passwordText = document.querySelector("#password");
   
-   passwordText.value = password;
-  
+      passwordText.value = password;
+    
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function passPrompts() {
+  passArr = [];
+
+  passLength = parseInt(prompt('Please choose a number between 8 - 128 for your desired password.'));
+    
+    while (isNaN(passLength) || passLength < 8 || passLength > 128) {
+    alert('Enter an integer between 8 - 128. Please try again.');
+    return false;
+  }
+
+  if (confirm('Do you want your new password to contain uppercase letters?')) {
+    passArr = passArr.concat(upperCase);
+  }
+  if (confirm('Do you want your new password to contain lowercase letters?')) {
+    passArr = passArr.concat(lowerCase);
+  }
+  if (confirm('Do you want your new password to contain special characters?')) {
+    passArr = passArr.concat(specChar);
+  }
+  if (confirm('Do you want your new password to contain numbers?')) {
+    passArr = passArr.concat(numeric);
+  }
+  return true;
+}
