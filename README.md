1ac392ff20a8611979cb71d1f4e4c170cd47959f
frvnci5 <frvncis@outlook.com>
2023-05-18 19:44:13 -0700

    finished adding comments to js file, updates to README

--- README.md
+++ README.md
@@ -1,4 +1,20 @@
-# JavaScript Password Generator
+# Week 3 Challenge: JavaScript Password Generator
+
+## Description
+
+This is a random password generator created using Javascript. 
+
+Upon vising the website, you will be prompted to choose your password criteria. That being whether you want uppercase letters, lowercase letters, special characters, and/ or numbers.
+
+Through this project I bettered my knowledge when it comes to the entirety of JavaScript. For example: 
+1: The importance of utilizing global and local scopes.
+2: For loops and if else statements.
+3: Creating functions that work seamlessly.
+
+This project gave me good practice that I know will be more than useful when it comes to creating future projects.
+
+## Deployed Page
+
 
 ## Credits 
 
--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment code here
+
 // Variable for password length and the password array.
-var passLength = 8;
+var passLength;
 var passArr = [];
 
 // Global variables for all available characters to be used in password generator. .split is used to turn the strings into arrays.
@@ -12,28 +13,22 @@ var numeric = '0123456789'.split('');
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-      var password = generatePassword();
-      var passwordText = document.querySelector("#password");
-  
-      passwordText.value = password;
-    
-}
-
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// Function to prompt user for what their password preferences are.
 function passPrompts() {
   passArr = [];
 
+// parseInt is used to make sure that what is chosen is an integer.
   passLength = parseInt(prompt('Please choose a number between 8 - 128 for your desired password.'));
-    
-    while (isNaN(passLength) || passLength < 8 || passLength > 128) {
+  
+  // isNaN is used to give an error if the user input something other than an integer.
+  if (isNaN(passLength) || passLength < 8 || passLength > 128) {
     alert('Enter an integer between 8 - 128. Please try again.');
-    return false;
+    return passPrompts();
   }
-
+  
   if (confirm('Do you want your new password to contain uppercase letters?')) {
     passArr = passArr.concat(upperCase);
   }
@@ -46,5 +41,24 @@ function passPrompts() {
   if (confirm('Do you want your new password to contain numbers?')) {
     passArr = passArr.concat(numeric);
   }
-  return true;
+}
+
+// Function to generate random password after user has chosen their specified prompts.
+function generatePassword() {
+  passPrompts();
+  var newPass = " ";
+  for (var i = 0; i < passLength; i++) {
+    var randomChoices = Math.floor(Math.random() * passArr.length);
+    newPass = newPass + passArr[randomChoices];
+  }
+  return newPass;
+}
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  
+   passwordText.value = password;
+  
 }
